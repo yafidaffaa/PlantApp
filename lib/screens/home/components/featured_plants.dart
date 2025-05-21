e13d118df2e71e4589a390bc3066adbcd58d4297
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-05-22 01:06:51 +0700

    menambahkan tempat untuk memanggil class card feature plants

--- lib/screens/home/components/featured_plants.dart
+++ lib/screens/home/components/featured_plants.dart
@@ -2,6 +2,29 @@ import 'package:flutter/material.dart';
 
 import '../../../constants.dart';
 
+class FeaturedPlants extends StatelessWidget {
+  const FeaturedPlants({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: <Widget>[
+          FeaturePlantCard(
+            image: "assets/images/bottom_img_1.png",
+            press: () {},
+          ),
+          FeaturePlantCard(
+            image: "assets/images/bottom_img_2.png",
+            press: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class FeaturePlantCard extends StatelessWidget {
   const FeaturePlantCard({Key? key, required this.image, required this.press})
     : super(key: key);
