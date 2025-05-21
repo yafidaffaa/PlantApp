32edc0e9d2e10f7a3e25d5b65d9d9fb0235155ac
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-05-22 01:04:51 +0700

    membuat card untuk feature plant

---
+++ lib/screens/home/components/featured_plants.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+
+class FeaturePlantCard extends StatelessWidget {
+  const FeaturePlantCard({Key? key, required this.image, required this.press})
+    : super(key: key);
+  final String image;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        margin: EdgeInsets.only(
+          left: kDefaultPadding,
+          top: kDefaultPadding / 2,
+          bottom: kDefaultPadding / 2,
+        ),
+        width: size.width * 0.8,
+        height: 185,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          image: DecorationImage(fit: BoxFit.cover, image: AssetImage(image)),
+        ),
+      ),
+    );
+  }
+}
