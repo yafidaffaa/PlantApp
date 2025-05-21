5d975610ea902c55ebaab0061550cf889ab393ef
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-05-22 01:01:30 +0700

    menambahkan class sebagai wadah untuk class card plants

--- lib/screens/home/components/recomend_plants.dart
+++ lib/screens/home/components/recomend_plants.dart
@@ -1,6 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app/screens/details/details_screen.dart';
 
 import '../../../constants.dart';
+
+class RecomendsPlants extends StatelessWidget {
+  const RecomendsPlants({required Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: <Widget>[
+          RecomendPlantCard(
+            image: "assets/images/image_1.png",
+            title: "Samantha",
+            country: "Russia",
+            price: 440,
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DetailsScreen()),
+              );
+            },
+          ),
+          RecomendPlantCard(
+            image: "assets/images/image_2.png",
+            title: "Angelica",
+            country: "Russia",
+            price: 440,
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DetailsScreen()),
+              );
+            },
+          ),
+          RecomendPlantCard(
+            image: "assets/images/image_3.png",
+            title: "Samantha",
+            country: "Russia",
+            price: 440,
+            press: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
 
 class RecomendPlantCard extends StatelessWidget {
   const RecomendPlantCard({
