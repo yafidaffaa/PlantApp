d195340e7d0c80864490f3f89adcd2509a00ea8d
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-05-22 00:58:49 +0700

    menambahkan card rekomendasi

---
+++ lib/screens/home/components/recomend_plants.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+
+class RecomendPlantCard extends StatelessWidget {
+  const RecomendPlantCard({
+    Key? key,
+    required this.image,
+    required this.title,
+    required this.country,
+    required this.price,
+    required this.press,
+  }) : super(key: key);
+
+  final String image, title, country;
+  final int price;
+  final VoidCallback? press;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.only(
+        left: kDefaultPadding,
+        top: kDefaultPadding / 2,
+        bottom: kDefaultPadding * 2.5,
+      ),
+      width: size.width * 0.4,
+      child: Column(
+        children: <Widget>[
+          Image.asset(image),
+          GestureDetector(
+            onTap: press,
+            child: Container(
+              padding: EdgeInsets.all(kDefaultPadding / 2),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 10),
+                    blurRadius: 50,
+                    color: kPrimaryColor.withOpacity(0.23),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
