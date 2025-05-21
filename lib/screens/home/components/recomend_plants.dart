27fc5215a9b3244e73fa3e1234abc9714d048f66
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-05-22 01:32:35 +0700

    membuat detail screen yang didalamnya ada body

--- lib/screens/home/components/recomend_plants.dart
+++ lib/screens/home/components/recomend_plants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:plant_app/screens/details/details_screen.dart';
+import 'package:plantapp/screens/details/details_screen.dart';
 
 import '../../../constants.dart';
 
