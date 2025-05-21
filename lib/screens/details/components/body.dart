c1d2aad2f6f635bfd6bd48c8d7224b3379df4032
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-05-22 01:31:19 +0700

    membuat class body

---
+++ lib/screens/details/components/body.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:plantapp/constants.dart';
+
+import 'image_and_icons.dart';
+import 'title_and_price.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          ImageAndIcons(key: UniqueKey(), size: size),
+          TitleAndPrice(
+            key: UniqueKey(),
+            title: "Angelica",
+            country: "Russia",
+            price: 440,
+          ),
+          SizedBox(height: kDefaultPadding),
+          Row(
+            children: <Widget>[
+              SizedBox(
+                width: size.width / 2,
+                height: 84,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: kPrimaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    "Buy Now",
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(onPressed: () {}, child: Text("Description")),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
